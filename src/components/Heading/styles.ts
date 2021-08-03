f23e769ec971f9d6343de688fca93a28c5fb6758
Pedro Duca <pedro.duca@sinqia.com.br>
2021-08-03 11:00:01 -0300

    Update component Heading

---
+++ src/components/Heading/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+import colors from '../../styles/colors';
+
+export const Container = styled.View`
+
+`;
+
+export const HeadingText = styled.Text`
+  padding-top: 16px;
+  color: ${colors.black};
+  font-family: roboto_500;
+  font-size: 30px;
+`
