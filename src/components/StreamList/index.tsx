ebbc3e0498fe4f3a566019c041f97605fce8d5cb
Pedro Duca <testandoduca@gmail.com>
2021-08-04 00:22:48 -0300

    add components: ChannelList, StreamList and Title

---
+++ src/components/StreamList/index.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+import streamThumbnail from "../../images/stream_thumbnail.jpg";
+
+import {
+  List,
+  StreamContainer,
+  StreamRow,
+  StreamThumbnail,
+  StreamColumn,
+  StreamHeader,
+  StreamAvatar,
+  StreamUsername,
+  StreamDescription,
+  StreamCategory,
+  TagRow,
+  TagView,
+  TagText,
+} from "./styles";
+
+const StreamList: React.FC = () => {
+  const StreamItem = () => (
+    <StreamContainer>
+      <StreamThumbnail source={streamThumbnail} />
+
+      <StreamRow>
+        <StreamColumn>
+          <StreamHeader>
+            <StreamAvatar />
+            <StreamUsername numberOfLines={1}>pedrohduca</StreamUsername>
+          </StreamHeader>
+
+          <StreamDescription numberOfLines={1}>
+            React Native Tutorial for Beginners
+          </StreamDescription>
+
+          <StreamCategory numberOfLines={1}>Technology</StreamCategory>
+        </StreamColumn>
+
+        <TagRow>
+          <TagView>
+            <TagText>Portuguese</TagText>
+          </TagView>
+          <TagView>
+            <TagText>Web Development</TagText>
+          </TagView>
+        </TagRow>
+      </StreamRow>
+    </StreamContainer>
+  );
+
+  return (
+    <List>
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+    </List>
+  );
+};
+
+export default StreamList;
